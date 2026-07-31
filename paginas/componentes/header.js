995afc83cc5f1a8fb14def2453c029677732cc60
natalhair <natalhairsite@gmail.com>
2026-07-31 16:44:31 -0300

    Create header.js

---
+++ paginas/componentes/header.js
@@ -0,0 +1,34 @@
+class SiteHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <style>
+                .inner-header {
+                    width: 100%;
+                    background: var(--grad-red-black);
+                    padding: 15px 5%;
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                }
+
+                .inner-logo {
+                    max-width: 140px;
+                }
+            </style>
+
+            <header class="inner-header">
+                <a href="../index.html">
+                    <img src="../logo.png" alt="Natal Hair" class="inner-logo">
+                </a>
+                <button id="open-menu" class="hamburger-btn" aria-label="Abrir Menu">
+                    <div class="hamburger-circle">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </button>
+            </header>
+        `;
+    }
+}
+customElements.define('site-header', SiteHeader);
